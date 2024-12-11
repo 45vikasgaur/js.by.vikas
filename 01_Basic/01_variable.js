66195304058eab73880f8ae7daec7f12217cc4f0
vikasgaur455 <gondvikas455@gmail.com>
2024-12-11 17:14:55 +0000

    conversion of datatype

---
+++ 01_Basic/01_variable.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "gondvikas383@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2 
+
+accountEmail = "vk@c.com"
+accountPassword = "789789"
+accountCity = "lucknow"
+
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
---
+++ 01_Basic/03_conversionOperation.js
@@ -0,0 +1,18 @@
+let score = 1
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);  
+
+// "33" => 33 (string)
+// "33anc" => NaN (undefined)
+// true => false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isloggedIn)
+console.log(booleanIsLoggedIn)
+console.log(typeof booleanIsLoggedIn)
