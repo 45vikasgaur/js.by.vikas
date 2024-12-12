f444b92fb92c28d6bb85015e288508a6f6471328
vikasgaur455 <gondvikas455@gmail.com>
2024-12-12 06:30:23 +0000

    Dtatatypes-summary.js

--- 01_Basic/03_conversionOperation.js
+++ 01_Basic/03_conversionOperation.js
@@ -1,4 +1,4 @@
-let score = 1
+// let score = 33
 
 // console.log(typeof score);
 // console.log(typeof (score));
@@ -11,8 +11,59 @@ let score = 1
 // "33anc" => NaN (undefined)
 // true => false => 0
 
-let isLoggedIn = 1
 
-let booleanIsLoggedIn = Boolean(isloggedIn)
-console.log(booleanIsLoggedIn)
-console.log(typeof booleanIsLoggedIn)
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// **************operation**********
+
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+// let str1 = "Hello"
+// let str2  = " vikas"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 2) * 5 % 3);
+
+
+
+// console.log(+true);
+// console.log(+"");
+
+// let x = 3;
+// const y = x++;
+
+// console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+
+// let num1,num2, num3
+
+// num1 = num2 = num3 = 2 + 2
+
+// let gameCounter = 100
+// ++gameCounter;
+// console.log(gameCounter);
+
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+
+
